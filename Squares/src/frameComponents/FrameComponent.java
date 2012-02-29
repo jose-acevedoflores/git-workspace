7727470e6c89a2eced7cdc5adbeed5185b2b88b0
Jose <acevedoflores.jose@gmail.com>
2012-02-29 00:14:54 -0400

    Added the frameComponenet class

---
+++ Squares/src/frameComponents/FrameComponent.java
@@ -0,0 +1,20 @@
+package frameComponents;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+public class FrameComponent extends JComponent{
+
+	private static final long serialVersionUID = 2L;
+	
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+	}
+	
+
+}
--- Squares/src/numberedSquare/NumberedSquare.java
+++ Squares/src/numberedSquare/NumberedSquare.java
@@ -1,5 +1,22 @@
 package numberedSquare;
 
-public class NumberedSquare {
+import java.awt.Rectangle;
 
+public class NumberedSquare extends Rectangle{
+
+	private static final long serialVersionUID = 1L;
+	private int number;
+	
+	
+	public NumberedSquare(int number)
+	{
+		super(30,30);
+		this.number = number;
+	}
+	
+	public int getNumber()
+	{
+		return number;
+	}
+	
 }
