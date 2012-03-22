fdf16794b8ccbfc58ba1da3c438c4cd43bb60beb
Jose <acevedoflores.jose@gmail.com>
2012-03-21 23:00:48 -0400

    Coded some test

--- UDP/src/udp/simple/server/Test.java
+++ UDP/src/udp/simple/server/Test.java
@@ -1,13 +1,32 @@
 package udp.simple.server;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Test {
 
 	/**
 	 * @param args
+	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException {
+		
+		
+		InetAddress w = InetAddress.getByName("localhost");
+		InetAddress p = InetAddress.getLocalHost();
+		
+		System.out.println("P - test\n");
+		
+		System.out.println(p);
+		System.out.println("Is Loopback address? "+ p.isLoopbackAddress());
+		System.out.println("HostName "+ p.getHostName());
+		
+		System.out.println("\nW - test\n");
+		
+		System.out.println(w);
+		System.out.println("Is Loopback address? "+ w.isLoopbackAddress());
+		System.out.println("HostName "+ w.getHostName());
+		
 	}
 
 }
