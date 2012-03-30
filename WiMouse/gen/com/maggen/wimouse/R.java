465f9df0d397b9349b35e2f66fda7c720aa86918
Jose <acevedoflores.jose@gmail.com>
2012-03-29 22:30:25 -0400

    -_-

--- WiMouse/gen/com/maggen/wimouse/R.java
+++ WiMouse/gen/com/maggen/wimouse/R.java
@@ -8,9 +8,6 @@
 package com.maggen.wimouse;
 
 public final class R {
-    public static final class array {
-        public static final int previousIP=0x7f040000;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -18,20 +15,20 @@ public final class R {
         public static final int r2d2=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f060005;
-        public static final int etIP1=0x7f060000;
-        public static final int etIP2=0x7f060001;
-        public static final int etIP3=0x7f060002;
-        public static final int etIP4=0x7f060003;
-        public static final int lvPreviousIP=0x7f060004;
+        public static final int button1=0x7f050005;
+        public static final int etIP1=0x7f050000;
+        public static final int etIP2=0x7f050001;
+        public static final int etIP3=0x7f050002;
+        public static final int etIP4=0x7f050003;
+        public static final int lvPreviousIP=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int buttonText=0x7f050003;
-        public static final int etNewIP=0x7f050002;
-        public static final int menu=0x7f050000;
+        public static final int app_name=0x7f040001;
+        public static final int buttonText=0x7f040003;
+        public static final int etNewIP=0x7f040002;
+        public static final int menu=0x7f040000;
     }
 }
