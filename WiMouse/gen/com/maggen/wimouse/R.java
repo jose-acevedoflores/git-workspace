8e8d084ae774fbdd0d814e51819791379345c33b
Jose <acevedoflores.jose@gmail.com>
2012-03-29 22:37:52 -0400

    Comments

--- WiMouse/gen/com/maggen/wimouse/R.java
+++ WiMouse/gen/com/maggen/wimouse/R.java
@@ -15,7 +15,7 @@ public final class R {
         public static final int r2d2=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f050005;
+        public static final int bStart=0x7f050005;
         public static final int etIP1=0x7f050000;
         public static final int etIP2=0x7f050001;
         public static final int etIP3=0x7f050002;
--- WiMouse/src/com/maggen/wimouse/WiMouseActivity.java
+++ WiMouse/src/com/maggen/wimouse/WiMouseActivity.java
@@ -2,13 +2,43 @@ package com.maggen.wimouse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class WiMouseActivity extends Activity {
+	
+	private Button startButton;
+	private EditText[] ipFields;
+	private ListView previousIPs;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Set what is going to be displayed to the user.
         setContentView(R.layout.main);
+        
+        //Initialize the variables.
+        this.initializeVars();
+    }
+    
+    private void initializeVars()
+    {
+    	//Initialize the button
+    	this.startButton = (Button) findViewById(R.id.bStart);
+    	
+    	//Initialize the list view
+    	this.previousIPs = (ListView) findViewById(R.id.lvPreviousIP);
+    	
+    	//Initialize the four ip fields.
+    	this.ipFields = new EditText[4];
+    	this.ipFields[0] = (EditText) findViewById(R.id.etIP1);
+    	this.ipFields[1] = (EditText) findViewById(R.id.etIP2);
+    	this.ipFields[2] = (EditText) findViewById(R.id.etIP3);
+    	this.ipFields[3] = (EditText) findViewById(R.id.etIP4);
+
+
     }
     
     public void onPause()
