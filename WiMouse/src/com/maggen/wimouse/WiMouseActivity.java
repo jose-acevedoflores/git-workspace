e5a5b1a154ae2de295550b523d58c2f41c68804a
Jose <acevedoflores.jose@gmail.com>
2012-03-29 21:21:15 -0400

    Initial commit

---
+++ WiMouse/src/com/maggen/wimouse/WiMouseActivity.java
@@ -0,0 +1,13 @@
+package com.maggen.wimouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WiMouseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
