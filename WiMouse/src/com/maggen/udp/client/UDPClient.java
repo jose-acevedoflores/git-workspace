dd34c9264532c36e34846623e9b35aed6bc6179a
Jose <acevedoflores.jose@gmail.com>
2012-03-31 00:18:20 -0400

    Implementing UDPClient

--- WiMouse/src/com/maggen/udp/client/UDPClient.java
+++ WiMouse/src/com/maggen/udp/client/UDPClient.java
@@ -1,23 +1,51 @@
 package com.maggen.udp.client;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class UDPClient {
 
-	DatagramSocket socket;
+	private DatagramSocket socket;
+	private InetAddress address;
+	private int port;
 	
-	public UDPClient()
+	public UDPClient(String ip)
 	{
+		//default port
+		port = 9876;
 		try {
 			socket = new DatagramSocket();
+			address = InetAddress.getByName(ip);
 		} catch (SocketException e) {
 			
 			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
 		}
+		
+		
 	}
 	
-	
+	public void updatePointer(int x , int y)
+	{
+		byte[] data = new byte[1024];
+		String str = x+","+y;
+		
+		data = str.getBytes();
+		
+		DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+		
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 }
