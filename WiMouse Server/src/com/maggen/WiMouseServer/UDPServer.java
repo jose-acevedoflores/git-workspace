157c00815f03c32b3f5720b001b6c31cee6c5a42
Jose <acevedoflores.jose@gmail.com>
2012-03-31 00:30:31 -0400

    WiMouse Server added

---
+++ WiMouse Server/src/com/maggen/WiMouseServer/UDPServer.java
@@ -0,0 +1,39 @@
+package com.maggen.WiMouseServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UDPServer {
+	
+	private DatagramSocket socket;
+	private int port;
+	
+	public UDPServer()
+	{
+		//Default
+		port = 9876;
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public String start()
+	{
+		byte[] dataReceived = new byte[1024];
+		DatagramPacket packet = new DatagramPacket(dataReceived, dataReceived.length);
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return new String(packet.getData());
+	}
+	
+
+}
