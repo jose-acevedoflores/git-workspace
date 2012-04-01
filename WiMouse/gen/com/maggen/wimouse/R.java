e06153f9e59d9ec9625aefbe9efce40c9336e20d
Jose <acevedoflores.jose@gmail.com>
2012-03-31 20:03:14 -0400

    Adding settings

--- WiMouse/gen/com/maggen/wimouse/R.java
+++ WiMouse/gen/com/maggen/wimouse/R.java
@@ -15,29 +15,32 @@ public final class R {
         public static final int r2d2=0x7f020001;
     }
     public static final class id {
-        public static final int about=0x7f060007;
-        public static final int bStart=0x7f060006;
-        public static final int etIP1=0x7f060001;
-        public static final int etIP2=0x7f060002;
-        public static final int etIP3=0x7f060003;
-        public static final int etIP4=0x7f060004;
-        public static final int exit=0x7f060009;
-        public static final int lvPreviousIP=0x7f060005;
-        public static final int settings=0x7f060008;
-        public static final int tvAbout=0x7f060000;
+        public static final int about=0x7f070007;
+        public static final int bStart=0x7f070006;
+        public static final int etIP1=0x7f070001;
+        public static final int etIP2=0x7f070002;
+        public static final int etIP3=0x7f070003;
+        public static final int etIP4=0x7f070004;
+        public static final int exit=0x7f070009;
+        public static final int lvPreviousIP=0x7f070005;
+        public static final int settings=0x7f070008;
+        public static final int tvAbout=0x7f070000;
     }
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int about=0x7f040004;
-        public static final int app_name=0x7f040001;
-        public static final int buttonText=0x7f040003;
-        public static final int etNewIP=0x7f040002;
-        public static final int menu=0x7f040000;
+        public static final int about=0x7f050004;
+        public static final int app_name=0x7f050001;
+        public static final int buttonText=0x7f050003;
+        public static final int etNewIP=0x7f050002;
+        public static final int menu=0x7f050000;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
---
+++ WiMouse/src/com/maggen/wimouse/menu/Settings.java
@@ -0,0 +1,9 @@
+package com.maggen.wimouse.menu;
+
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+	
+	
+
+}
