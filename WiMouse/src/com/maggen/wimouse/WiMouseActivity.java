d4a3e10dd98c766544d0a5d3881a78b2f6e3bd30
Jose <acevedoflores.jose@gmail.com>
2012-04-03 16:06:56 -0400

    CheckingAssets method needs to be re-written as one cannot write to the assets folder

--- WiMouse/src/com/maggen/wimouse/WiMouseActivity.java
+++ WiMouse/src/com/maggen/wimouse/WiMouseActivity.java
@@ -1,11 +1,17 @@
 package com.maggen.wimouse;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,8 +26,13 @@ public class WiMouseActivity extends Activity {
 
 	private Button startButton;
 	private EditText[] ipFields;
+	
 	private ListView previousIPs;
+	private String ipHistory;
+	
 	private AlertDialog.Builder dia;
+	private AssetManager history;
+
 	public static String ip ="";
 	public static TextView info;
 
@@ -33,14 +44,37 @@ public class WiMouseActivity extends Activity {
 		setContentView(R.layout.main);
 
 		this.checkAssets();
-		
+
 		//Initialize the variables.
 		this.initializeVars();
 	}
-	
+
 	private void checkAssets()
 	{
-		
+		InputStream in=null;
+		//Initialize a string writer to save the data from the input stream
+		StringWriter sw= new StringWriter(100);
+		//Get the assets of this application 
+		history = this.getAssets();
+		try{
+			//Check the file history.txt (IOexception)
+			in = this.history.open("history.txt");
+			int n;
+			
+			//While in.read() is not -1 (end of input stream)
+			while((n = in.read()) != -1)
+				//Write to the string writer the character found.
+				sw.write(n);
+			
+			//Close the stream.
+			in.close();
+			this.ipHistory = sw.toString();
+			Log.d("History", "Content: "+this.ipHistory);
+		}
+		catch (IOException e) {
+			Log.d("CheckAsset", "File not found");
+		}
+
 	}
 
 	private void initializeVars()
@@ -61,11 +95,11 @@ public class WiMouseActivity extends Activity {
 
 		// initialize the alert dialog just in case.
 		dia = new AlertDialog.Builder(this);
-		
+
 		//Get the preference to access the port 
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
 		String port = pref.getString("port", "9876");
-		
+
 		//Initialize the field to show the port
 		info = (TextView) findViewById(R.id.tvInfo);
 		info.setText(info.getText() +" Port: "+port);
@@ -75,19 +109,19 @@ public class WiMouseActivity extends Activity {
 	{
 		super.onPause();
 	}
-		 
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		MenuInflater inflater = this.getMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
-		
+
 		return true;
 	}
-	
-    public boolean onOptionsItemSelected(MenuItem item) 
-    {
-    	switch (item.getItemId()) {
+
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		switch (item.getItemId()) {
 		case R.id.about:
 			Intent i = new Intent("com.maggen.wimouse.menu.ABOUT");
 			startActivity(i);
@@ -97,16 +131,16 @@ public class WiMouseActivity extends Activity {
 			Intent settings = new Intent("com.maggen.wimouse.menu.SETTINGS");
 			startActivity(settings);
 			break;
-			
+
 		case R.id.exit:
 			this.finish();
 			break;
-			
+
 		default:
 			break;
 		}
-    	return true;
-    }
+		return true;
+	}
 
 	/*-------------------------Private class -------------------------------------*/
 
