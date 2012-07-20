00a4a32cec1e7bcd242d1f182a2988efe3d35811
Jose <jose@res364d-128-61-69-144.res.gatech.edu>
2012-07-20 15:12:45 -0400

    Added BaseXNumber class to the utils package

--- Binary Converter/src/binary/converter/MainActivity.java
+++ Binary Converter/src/binary/converter/MainActivity.java
@@ -3,8 +3,7 @@ package binary.converter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+
 
 public class MainActivity extends Activity {
 
