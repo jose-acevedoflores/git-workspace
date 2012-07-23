998fdb6a61bf3459f9ef783aab2c3250d0c51386
Jose <jose@r69h144.res.gatech.edu>
2012-07-22 23:48:51 -0400

    Added the multiplicativeInverse algorithm

--- Utils/src/euclideanalgo/util/Tester.java
+++ Utils/src/euclideanalgo/util/Tester.java
@@ -17,10 +17,12 @@ public class Tester {
 		System.out.println("s = "+res[1]);
 		System.out.println("t = "+res[2]);
 		
-		System.out.println("Mult inverse: "+GreatestCommonDivisor.multiplicativeInverse(23, 100));
+		System.out.println("Mult inverse: "+GreatestCommonDivisor.multiplicativeInverse(4, 16));
 
 		//Figure 2.26 
 		System.out.println((3*15+5*23+7*15+2*24)%26);
+		
+		System.out.println(-7%16);
 	}
 
 }
