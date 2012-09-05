64dadaca764ab070cf3cd2fb3a33c84cddbf4b68
Jose <jose@Joses-MacBook.local>
2012-09-04 20:17:59 -0400

    s

---
+++ WiMouseICS/src/com/maggen/wimouse/Menu.java
@@ -0,0 +1,15 @@
+package com.maggen.wimouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity{
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.)
+
+	}
+	
+}
+
