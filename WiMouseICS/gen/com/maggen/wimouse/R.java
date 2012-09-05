69bd34bc93aeea1c0ac54b24bac7b164ac7e521e
Jose <jose@Joses-MacBook.local>
2012-09-04 20:11:21 -0400

    s

--- WiMouseICS/gen/com/maggen/wimouse/R.java
+++ WiMouseICS/gen/com/maggen/wimouse/R.java
@@ -10,38 +10,46 @@ package com.maggen.wimouse;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int r2d2=0x7f020001;
     }
     public static final class id {
-        public static final int about=0x7f070008;
-        public static final int bStart=0x7f070006;
-        public static final int etIP1=0x7f070002;
-        public static final int etIP2=0x7f070003;
-        public static final int etIP3=0x7f070004;
-        public static final int etIP4=0x7f070005;
-        public static final int exit=0x7f07000a;
-        public static final int lvPreviousIP=0x7f070007;
-        public static final int settings=0x7f070009;
-        public static final int tvAbout=0x7f070000;
-        public static final int tvInfo=0x7f070001;
+        public static final int about=0x7f090008;
+        public static final int bStart=0x7f090006;
+        public static final int etIP1=0x7f090002;
+        public static final int etIP2=0x7f090003;
+        public static final int etIP3=0x7f090004;
+        public static final int etIP4=0x7f090005;
+        public static final int exit=0x7f09000a;
+        public static final int lvPreviousIP=0x7f090007;
+        public static final int settings=0x7f090009;
+        public static final int tvAbout=0x7f090000;
+        public static final int tvInfo=0x7f090001;
     }
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int menu=0x7f060000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int about=0x7f050004;
-        public static final int app_name=0x7f050001;
-        public static final int buttonText=0x7f050003;
-        public static final int etNewIP=0x7f050002;
-        public static final int menu=0x7f050000;
-        public static final int pref=0x7f050006;
-        public static final int settings=0x7f050005;
+        public static final int about=0x7f060004;
+        public static final int app_name=0x7f060001;
+        public static final int buttonText=0x7f060003;
+        public static final int etNewIP=0x7f060002;
+        public static final int menu=0x7f060000;
+        public static final int pref=0x7f060006;
+        public static final int settings=0x7f060005;
+    }
+    public static final class style {
+        public static final int AppTheme=0x7f070000;
     }
     public static final class xml {
         public static final int preference_headers=0x7f040000;
